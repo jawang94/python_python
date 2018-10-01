d73eced92f140dd709abc9ec70335dda60c4a42d
Jason Wang <wang.jason.010@gmail.com>
2018-10-01 09:09:58 -0700

    intermediate functions

---
+++ python_fundamentals/intermediate_functions_1.py
@@ -0,0 +1,25 @@
+# Create beCheerful().  Within it, print string "good morning!" 98 times.
+def beCheerful(name="",repeat=98):
+    print(f"Good morning {name}!"*repeat)
+beCheerful()
+
+import random
+
+return random.random()*50
+
+return random.random()*100
+
+import random
+x = random.random()*50
+def randomreturn():
+  return int(x + 50)
+print(randomreturn())
+
+import random
+def randomreturn():
+  x = random.random()*500
+  if(x >= 50):
+    return int(x + 50)
+  else:
+    return 50 + int(x)
+print(randomreturn())
