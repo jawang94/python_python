e1686acdec5941080a2e4bd05e370219c8f593d2
Jason Wang <wang.jason.010@gmail.com>
2018-10-02 16:12:06 -0700

    tdd update

--- python_fundamentals/intro_to_tdd.py
+++ python_fundamentals/intro_to_tdd.py
@@ -1,6 +1,5 @@
-import unittest
-
 # 1
+import unittest
 def reverseList(arr):
     for i in range(0, len(arr)):
         temp = arr[i]
@@ -28,9 +27,10 @@ if __name__ == "__main__":
 
 
 # 2
+import unittest
 def isPalindrome(str):
     newstr = ""
-    for i in range(0,len(str)):
+    for i in range(0, len(str)):
         newstr += str[len(str)-1-i]
     return newstr == str
 
@@ -47,6 +47,50 @@ class isPalindromeTest(unittest.TestCase):
 
     def test4(self):
         return self.assertTrue(isPalindrome("poop"))
+
+    def setUp(self):
+        print("Running setup")
+        print("*"*50)
+
+    def tearDown(self):
+        print("Running teardown")
+        print("*"*50)
+
+
+if __name__ == "__main__":
+    unittest.main()
+
+
+# 3
+import unittest
+def countChange(num):
+    # pass
+    change = [0, 0, 0, 0]
+    change[0] = int((num-num%25)/25)
+    num = num%25
+    change[1] = int((num-num%10)/10)
+    num = num%10
+    change[2] = int((num-num%5)/5)
+    num = num%5
+    change[3] = num
+    return change
+
+
+class countChangeTest(unittest.TestCase):
+    def test1(self):
+        return self.assertEqual(countChange(87), [3,1,0,2])
+
+    def test2(self):
+        return self.assertEqual(countChange(100), [4,0,0,0])
+
+    def test3(self):
+        return self.assertEqual(countChange(55), [2,0,1,0])
+
+    def test4(self):
+        return self.assertEqual(countChange(63), [2,1,0,3])
+
+    def test5(self):
+        return self.assertEqual(countChange(105), [4,0,1,0])
         
     def setUp(self):
         print("Running setup")
