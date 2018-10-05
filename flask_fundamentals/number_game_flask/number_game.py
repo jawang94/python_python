84c03aa5bc259b1718f1c3122e91ab7f492e8b85
Jason Wang <wang.jason.010@gmail.com>
2018-10-05 15:30:17 -0700

    great number game

---
+++ flask_fundamentals/number_game_flask/number_game.py
@@ -0,0 +1,42 @@
+from flask import Flask, render_template, session, redirect, request
+app = Flask(__name__)
+app.secret_key = 'datsecret' 
+
+@app.route("/")
+def main():
+    return render_template("number_game.html")
+
+@app.route('/submit', methods=["post"])
+def submit():
+    import random
+    x = random.randrange(0, 101) 
+    guess = request.form['guess']
+    session['result'] = ""
+    session['bgrcolor'] = ""
+    if x == int(guess):
+        return redirect("/correct")
+    elif x > int(guess):
+        return redirect("/toolow")
+    else:
+        return redirect("/toohigh")
+
+@app.route('/correct')
+def correct():
+    session['result'] = "Correct"
+    session['bgrcolor'] = "Green"
+    return render_template("number_game_result.html", result=session['result'],bgrcolor=session['bgrcolor'])
+
+@app.route('/toolow')
+def toolow():
+    session['result'] = "Too Low!"
+    session['bgrcolor'] = "lightblue"
+    return render_template("number_game_result.html", result=session['result'],bgrcolor=session['bgrcolor'])
+
+@app.route('/toohigh')
+def toohigh():
+    session['result'] = "Too High!"
+    session['bgrcolor'] = "Red"
+    return render_template("number_game_result.html", result=session['result'],bgrcolor=session['bgrcolor'])
+
+if __name__ == "__main__":
+    app.run(debug=True)
