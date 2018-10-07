ad319bde78348211dd2061104167f4eb25803ffc
Jason Wang <wang.jason.010@gmail.com>
2018-10-07 11:47:01 -0700

    insert sort update

--- python_fundamentals/insert_sort.py
+++ python_fundamentals/insert_sort.py
@@ -1,12 +1,26 @@
+#JAVASCRIPT SOLUTION:
+# function insertSort(arr){
+#   for(var i=0 ; i<arr.length ; i++){
+#     let value = arr[i];
+#     for(var j=i-1 ; j>=0 && arr[j] > value ; j--){
+#       arr[j+1] = arr[j];
+#     }
+#     arr[j+1] = value;
+#   }
+#   return arr;
+# } 
+
+let coolarr = [1,5,7,3,6];
+insertSort(coolarr);
+
 def insertSort(arr):
-    for i in range(1,len(arr)):
+    for i in range(0,len(arr)):
         value = arr[i]
-        j = i-1
-        while j >= 0 and value < arr[j]:
-          arr[j+1] = arr[j]
-          j -= 1
+        for j in range(i-1,-1):
+            if arr[j] > value:
+                arr[j+1] = arr[j]
         arr[j+1] = value
     return arr
 
 coolarr = [1,5,7,3,6]
-print(insertSort(coolarr))
+insertSort(coolarr)
