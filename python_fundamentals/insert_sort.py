e314fd8ede822f245719be3111a9ed2ec845b12e
Jason Wang <wang.jason.010@gmail.com>
2018-10-08 08:54:34 -0700

    insert sort update

--- python_fundamentals/insert_sort.py
+++ python_fundamentals/insert_sort.py
@@ -14,13 +14,14 @@ let coolarr = [1,5,7,3,6];
 insertSort(coolarr);
 
 def insertSort(arr):
-    for i in range(0,len(arr)):
+    for i in range(1,len(arr)):
         value = arr[i]
-        for j in range(i-1,-1):
-            if arr[j] > value:
-                arr[j+1] = arr[j]
+        j = i-1
+        while j >= 0 and value < arr[j]:
+          arr[j+1] = arr[j]
+          j -= 1
         arr[j+1] = value
     return arr
 
 coolarr = [1,5,7,3,6]
-insertSort(coolarr)
+print(insertSort(coolarr))
