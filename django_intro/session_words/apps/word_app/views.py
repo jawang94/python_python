ff7f871c57f51fbb297d8e42c7ddf8f297ee0708
Jason Wang <wang.jason.010@gmail.com>
2018-10-10 19:11:59 -0700

    session words django

---
+++ django_intro/session_words/apps/word_app/views.py
@@ -0,0 +1,17 @@
+from django.shortcuts import render, redirect
+from time import gmtime, strftime
+
+def index(request):
+    return render(request, "word_app/index.html")
+
+def process(request):
+    if not request.session['words']:
+        request.session['words'] = []
+    temp_list = request.session['words']
+    temp_list.append({"word": request.POST['word'], "color": request.POST['color'], "show_big": request.POST['big_font'], "created_on": strftime("%Y-%m-%d %H:%M:%S", gmtime())})
+    request.session['words'] = temp_list
+    return redirect("/session_words")
+
+def clear(request):
+    request.session['words'] = ""
+    return redirect("/session_words")
