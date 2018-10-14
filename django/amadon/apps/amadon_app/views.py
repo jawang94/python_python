6ee86891e39abb1ff48cc0a8880abafc053a9c00
Jason Wang <wang.jason.010@gmail.com>
2018-10-14 15:27:48 -0700

    amadon project for django

--- django/amadon/apps/amadon_app/views.py
+++ django/amadon/apps/amadon_app/views.py
@@ -17,6 +17,21 @@ def create(request):
     return redirect("/amadon")
 
 def process(request):
+    if 'total' not in request.session:
+        request.session['total'] = 0
+    if 'quantity' not in request.session:
+        request.session['quantity'] = 0
+    quantity = request.POST['quantity']
+    item = Item.objects.get(id=request.POST['itemid'])
+    total = int(quantity) * float(item.item_price)
+    order_form = OrderForm(request.POST)
+    if order_form.is_valid():
+        order_form.order_total = total
+        order_form.description = str(f'Purchased {quantity} {item}s for ${total}.')
+        order_form.save()
+    request.session['currenttotal'] = round(float(total),4)
+    request.session['total'] += round(float(total),4)
+    request.session['quantity'] += int(quantity)
     return redirect("/amadon/checkout")
 
 def checkout(request):
